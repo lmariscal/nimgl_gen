2002f83fa36618f7b9cb31a0071d1936f18c75f2
Leonardo Mariscal <leo@cav.bz>
2018-09-25 01:20:40 -0500

    opengl generator fully functional, still need to add documentation since the wrappers are useless atm

--- opengl/src/opengl.nim
+++ opengl/src/opengl.nim
@@ -8,6 +8,10 @@ when not isMainModule:
 
 type
   Argument = tuple[ptype: string, name: string]
+  EnumVal = object
+    name: string
+    val: string
+    comment: string
   Command = object
     rtn: string
     name: string
@@ -18,6 +22,7 @@ const
 # Copyright 2018, NimGL contributors.
 
 import nimgl/glfw # used to get the glfwGetProcAddress procedure
+import strutils
 
 ## OpenGL Bindings
 ## ===
@@ -26,54 +31,155 @@ import nimgl/glfw # used to get the glfwGetProcAddress procedure
 ## with the opengl generator.
 
 type
-  GLvoid*           = pointer
-  GLeglImageOES*    = distinct pointer
-  GLsync*           = distinct pointer
-  ClContext*        = distinct pointer
-  ClEvent*          = distinct pointer
-  GLbyte*           = int8
-  GLshort*          = int16
-  GLintptr*         = int
-  GLintptrARB*      = int
-  GLsizeiptr*       = int
-  GLsizeiptrARB*    = int
-  GLclampx*         = int32
-  GLfixed*          = int32
-  GLint*            = int32
-  GLsizei*          = int32
+  GLvoid* = pointer
+  GLeglImageOES* = distinct pointer
+  GLsync* = distinct pointer
+  ClContext* = distinct pointer
+  ClEvent* = distinct pointer
+  GLeglClientBufferEXT* = distinct pointer
+  GLbyte* = int8
+  GLshort* = int16
+  GLintptr* = int
+  GLintptrARB* = int
+  GLsizeiptr* = int
+  GLsizeiptrARB* = int
+  GLclampx* = int32
+  GLfixed* = int32
+  GLint* = int32
+  GLsizei* = int32
   GLvdpauSurfaceNV* = int32
-  GLint64*          = int64
-  GLint64EXT*       = int64
-  GLubyte*          = uint8
-  GLhalf*           = uint16
-  GLhalfARB*        = uint16
-  GLhalfNV*         = uint16
-  GLushort*         = uint16
-  GLbitfield*       = uint32
-  GLhandleARB*      = uint32
-  GLenum*           = uint32
-  GLuint*           = uint32
-  GLuint64*         = uint64
-  GLuint64EXT*      = uint64
-  GLboolean*        = bool
-  GLchar*           = char
-  GLcharARB*        = byte
-  GLclampf*         = float32
-  GLfloat*          = float32
-  GLclampd*         = float64
-  GLdouble*         = float64
+  GLint64* = int64
+  GLint64EXT* = int64
+  GLubyte* = uint8
+  GLhalf* = uint16
+  GLhalfARB* = uint16
+  GLhalfNV* = uint16
+  GLushort* = uint16
+  GLbitfield* = uint32
+  GLhandleARB* = uint32
+  GLenum* = uint32
+  GLuint* = uint32
+  GLuint64* = uint64
+  GLuint64EXT* = uint64
+  GLboolean* = bool
+  GLchar* = char
+  GLcharARB* = byte
+  GLclampf* = float32
+  GLfloat* = float32
+  GLclampd* = float64
+  GLdouble* = float64
+  GLDEBUGPROC* = proc(source: GLenum, `type`: GLenum, id: GLuint, severity: GLenum, length: GLsizei, message: cstring, userParam: pointer): void {.cdecl.}
+  GLDEBUGPROCARB* = GLDEBUGPROC
+  GLDEBUGPROCKHR* = GLDEBUGPROC
+  GLDEBUGPROCAMD* = GLDEBUGPROC
+  GLVULKANPROCNV* = proc(): void {.cdecl.}
+  GLPROCLOADERPROC* = proc(name: cstring): pointer {.cdecl.}
 
 var
+  glVersionMajor*: int
+  glVersionMinor*: int
+
+# Constants
+const
+  GL_INVALID_INDEX*: uint32 = uint32(0xFFFFFFFF)
+  GL_TIMEOUT_IGNORED*: uint64 = uint64(0xFFFFFFFFFFFFFFFF)
+  GL_TIMEOUT_IGNORED_APPLE*: uint64 = uint64(0xFFFFFFFFFFFFFFFF)
 """
+  loader = """
+
+proc glGetProcAddress(name: cstring): pointer =
+  ## Gets a pointer to the procedure
+  ## If `release` is not defined it will check that the address has been found.
+  result = cglGetProcAddress(name)
+  when not defined(release):
+    if result == nil:
+      echo $name & " <- has not been found"
+      echo "are you sure an opengl context is running?"
+  """
   footer = """
 
-proc glInit*(loader: proc = getProcAddress) =
-  echo "init"
-"""
+proc glInit*(loader: GLPROCLOADERPROC = getProcAddress): bool =
+  ## Gets the opengl version that is supported and loads the proper functions
+  cglGetProcAddress = loader
 
-# TODO: remove the compatibility mode stuff from the generator ```<remove profile="core" comment="Compatibility-only GL 1.0 features removed from GL 3.2">``.
-# TODO: make the loader for all the procs
-# TODO: maybe separate them between versions for loading time
+  cglGetString = cast[proc (name: GLenum): ptr GLubyte {.cdecl.}](cglGetProcAddress("glGetString"))
+  if cglGetProcAddress == nil: return false
+
+  var glVersion = cast[cstring](glGetString(GL_VERSION))
+  if glVersion.isNil: return false
+
+  # Thanks to David Herberth who made this version verifier
+  var prefixes = ["OpenGL ES-CM ", "OpenGL ES-CL ", "OpenGL ES "]
+
+  var version: string = $glVersion
+  for p in prefixes:
+    if version.startsWith(p):
+      version = version.replace(p)
+      break
+
+  var major = ord(glVersion[0]) - ord('0')
+  var minor = ord(glVersion[2]) - ord('0')
+
+  glVersionMajor = major
+  glVersionMinor = minor
+
+  if (major == 1 and minor >= 0) or major > 1: load1_0()
+  if (major == 1 and minor >= 1) or major > 1: load1_1()
+  if (major == 1 and minor >= 2) or major > 1: load1_2()
+  if (major == 1 and minor >= 3) or major > 1: load1_3()
+  if (major == 1 and minor >= 4) or major > 1: load1_4()
+  if (major == 1 and minor >= 5) or major > 1: load1_5()
+  if (major == 2 and minor >= 0) or major > 2: load2_0()
+  if (major == 2 and minor >= 1) or major > 2: load2_1()
+  if (major == 3 and minor >= 0) or major > 3: load3_0()
+  if (major == 3 and minor >= 1) or major > 3: load3_1()
+  if (major == 3 and minor >= 2) or major > 3: load3_2()
+  if (major == 3 and minor >= 3) or major > 3: load3_3()
+  if (major == 4 and minor >= 0) or major > 4: load4_0()
+  if (major == 4 and minor >= 1) or major > 4: load4_1()
+  if (major == 4 and minor >= 2) or major > 4: load4_2()
+  if (major == 4 and minor >= 3) or major > 4: load4_3()
+  if (major == 4 and minor >= 4) or major > 4: load4_4()
+  if (major == 4 and minor >= 5) or major > 4: load4_5()
+  if (major == 4 and minor >= 6) or major > 4: load4_6()
+  return true
+"""
+  keywords = ["addr", "and", "as", "asm", "bind", "block", "break", "case", "cast", "concept", "const", "continue",
+              "converter", "defer", "discard", "distinct", "div", "do", "elif", "else", "end", "enum", "except",
+              "export", "finally", "for", "from", "func", "if", "import", "in", "include", "interface", "is", "isnot",
+              "iterator", "let", "macro", "method", "mixin", "mod", "nil", "not", "notin", "object", "of", "or", "out",
+              "proc", "ptr", "raise", "ref", "return", "shl", "shr", "static", "template", "try", "tuple", "type",
+              "using", "var", "when", "while", "xor", "yield"]
+  usedConstants = ["GL_BYTE", "GL_SHORT", "GL_INT", "GL_FLOAT", "GL_DOUBLE", "GL_FIXED"]
+  bannedConstants = ["GL_INVALID_INDEX", "GL_TIMEOUT_IGNORED", "GL_TIMEOUT_IGNORED_APPLE", "GL_ACTIVE_PROGRAM_EXT"]
+  banned = ["glCreateSyncFromCLeventARB", "glGetTransformFeedbacki_v"]
+
+var
+  enums: seq[EnumVal] = @[]
+  commands: seq[Command] = @[]
+  loaded: seq[string] = @[]
+  toload: seq[string] = @[]
+
+  load1_0: seq[string] = @[]
+  load1_1: seq[string] = @[]
+  load1_2: seq[string] = @[]
+  load1_3: seq[string] = @[]
+  load1_4: seq[string] = @[]
+  load1_5: seq[string] = @[]
+  load2_0: seq[string] = @[]
+  load2_1: seq[string] = @[]
+  load3_0: seq[string] = @[]
+  load3_1: seq[string] = @[]
+
+  load3_2: seq[string] = @[]
+  load3_3: seq[string] = @[]
+  load4_0: seq[string] = @[]
+  load4_1: seq[string] = @[]
+  load4_2: seq[string] = @[]
+  load4_3: seq[string] = @[]
+  load4_4: seq[string] = @[]
+  load4_5: seq[string] = @[]
+  load4_6: seq[string] = @[]
 
 proc getRegistry() =
   ## downloads the xml opengl registry
@@ -86,13 +192,48 @@ proc newCommand(): Command =
   result.rtn       = ""
   result.arguments = @[]
 
-proc toProc(cmd: Command): string =
-  result = "  " & cmd.name & "*: proc ("
+proc toProcVar(cmd: Command): string =
+  result = "  c" & cmd.name & ": proc ("
   for i in cmd.arguments:
     result.add(i.name & ": " & i.ptype & ", ")
   if cmd.arguments.len > 0:
     result = result.substr(0, result.len - 3)
-  result.add("): " & cmd.rtn & "{.cdecl.}\n")
+  result.add("): " & cmd.rtn & " {.cdecl.}\n")
+
+proc transKeys(cmd: string): string =
+  result = cmd
+  result = result.replace("const ", "")
+  result = result.replace("const", "")
+  result = result.replace(" *", "*")
+  result = result.replace("void*", "pointer")
+  result = result.replace("GLchar*", "cstring")
+
+  if result.contains('*'):
+    let levels = result.count('*')
+    result = result.replace("*", "")
+    for i in 0..<levels:
+      result = "ptr " & result
+
+proc getCmd(name: string): (Command, bool) =
+  for c in commands:
+    if c.name == name:
+      return (c, true)
+  return (commands[0], false)
+
+proc genLoader(load: seq[string], version: string): string =
+  result = "\nproc load" & version & "() =\n"
+  for name in load:
+    let (cmd, state) = getCmd(name)
+    if not state or loaded.contains(name): continue
+    result.add("  c" & cmd.name & " = cast[")
+    result.add("proc (")
+    for i in cmd.arguments:
+      result.add(i.name & ": " & i.ptype & ", ")
+    if cmd.arguments.len > 0:
+      result = result.substr(0, result.len - 3)
+    result.add("): " & cmd.rtn & " {.cdecl.}](glGetProcAddress(\"" & cmd.name & "\"))\n")
+    loaded.add(name)
+
 
 proc main() =
   if not fileExists("gl.xml"):
@@ -105,26 +246,153 @@ proc main() =
 
   outCode.add(header)
 
-  for i in file.parseXml.findAll("command"):
+  let xml = file.parseXml()
+
+  # opengl procedures
+  for i in xml.findAll("command"):
     var cmd  = newCommand()
 
+    # proc name
     if i.child("proto") == nil: continue
     cmd.name = i.child("proto").child("name").innerText
+    # return value
     cmd.rtn  = i.child("proto").innerText
     cmd.rtn  = cmd.rtn.substr(0, cmd.rtn.len - cmd.name.len - 1)
     if cmd.rtn.endsWith(" "):
       cmd.rtn = cmd.rtn.substr(0, cmd.rtn.len - 2)
+    cmd.rtn = transKeys(cmd.rtn)
 
+    # params
     for p in i.findAll("param"):
       var arg: Argument
       arg.name  = p.child("name").innerText
       arg.ptype = p.innerText
       arg.ptype = arg.ptype.substr(0, arg.ptype.len - arg.name.len - 1)
+
       if arg.ptype.endsWith(" "):
         arg.ptype = arg.ptype.substr(0, arg.ptype.len - 2)
+
+      var usingKeyword = false
+      for p in arg.name.split(" "):
+        if keywords.contains(p): usingKeyword = true
+
+      if usingKeyword:
+        var word = ""
+        for i in 0..<keywords.len:
+          if arg.name.contains(keywords[i]):
+            word = keywords[i]
+            break
+        arg.name = arg.name.replace(word, '`' & word & '`')
+
+      arg.ptype = transKeys(arg.ptype)
       cmd.arguments.add(arg)
 
-    outCode.add(cmd.toProc())
+    # add it
+    if not banned.contains(cmd.name) and not commands.contains(cmd):
+      commands.add(cmd)
+
+  # opengl consts
+  for e in xml.findAll("enums"):
+    for i in e.findAll("enum"):
+      if i.attr("name") == nil or i.attr("value") == "" or bannedConstants.contains(i.attr("name")): continue
+
+      var enumv: EnumVal
+      enumv.name = i.attr("name")
+      if usedConstants.contains(enumv.name):
+        enumv.name = "E" & enumv.name
+      enumv.val = i.attr("value")
+
+      if i.attr("comment") == "": enumv.comment = ""
+      else: enumv.comment = " ## " & i.attr("comment")
+      enums.add(enumv)
+
+  for f in xml.findAll("feature"):
+    let number = f.attr("number").parseFloat
+    var current: ptr seq[string]
+    if   number == 1.0: current = load1_0.addr
+    elif number == 1.1: current = load1_1.addr
+    elif number == 1.2: current = load1_2.addr
+    elif number == 1.3: current = load1_3.addr
+    elif number == 1.4: current = load1_4.addr
+    elif number == 1.5: current = load1_5.addr
+    elif number == 2.0: current = load2_0.addr
+    elif number == 2.1: current = load2_1.addr
+    elif number == 3.0: current = load3_0.addr
+    elif number == 3.1: current = load3_1.addr
+
+    elif number == 3.2: current = load3_2.addr
+    elif number == 3.3: current = load3_3.addr
+    elif number == 4.0: current = load4_0.addr
+    elif number == 4.1: current = load4_1.addr
+    elif number == 4.2: current = load4_2.addr
+    elif number == 4.3: current = load4_3.addr
+    elif number == 4.4: current = load4_4.addr
+    elif number == 4.5: current = load4_5.addr
+    elif number == 4.6: current = load4_6.addr
+
+    for req in f.findAll("require"):
+      for c in req.findAll("command"):
+        current[].add(c.attr("name"))
+        toload.add(c.attr("name"))
+
+    if number == 3.2:
+      for r in f.findAll("remove"):
+        for c in r.findAll("command"):
+          for i in 0 ..< commands.len - 1:
+            if commands[i].name == c.attr("name"):
+              commands.del(i)
+        for e in r.findAll("enum"):
+          for i in 0 ..< enums.len - 1:
+            if enums[i].name == e.attr("name"):
+              enums.del(i)
+
+  for e in enums:
+    outCode.add("  " & e.name & "*: GLenum = GLenum(" & e.val & ")" & e.comment & "\n")
+
+  outCode.add("\n\n# Loaded Procedures\nvar\n")
+  for cmd in commands:
+    if toload.contains(cmd.name):
+      outCode.add(cmd.toProcVar())
+  outCode.add("  cglGetProcAddress: GLPROCLOADERPROC\n")
+
+  outCode.add(loader)
+  outCode.add(genLoader(load1_0, "1_0"))
+  outCode.add(genLoader(load1_1, "1_1"))
+  outCode.add(genLoader(load1_2, "1_2"))
+  outCode.add(genLoader(load1_3, "1_3"))
+  outCode.add(genLoader(load1_4, "1_4"))
+  outCode.add(genLoader(load1_5, "1_5"))
+  outCode.add(genLoader(load2_0, "2_0"))
+  outCode.add(genLoader(load2_1, "2_1"))
+  outCode.add(genLoader(load3_0, "3_0"))
+  outCode.add(genLoader(load3_1, "3_1"))
+
+  outCode.add(genLoader(load3_2, "3_2"))
+  outCode.add(genLoader(load3_3, "3_3"))
+  outCode.add(genLoader(load4_0, "4_0"))
+  outCode.add(genLoader(load4_1, "4_1"))
+  outCode.add(genLoader(load4_2, "4_2"))
+  outCode.add(genLoader(load4_3, "4_3"))
+  outCode.add(genLoader(load4_4, "4_4"))
+  outCode.add(genLoader(load4_5, "4_5"))
+  outCode.add(genLoader(load4_6, "4_6"))
+
+  outCode.add("\n# Wrapper to add documentation and future manual modifications\n")
+  for cmd in commands:
+    if not toload.contains(cmd.name): continue
+    outCode.add("proc " & cmd.name & "*(")
+    for i in cmd.arguments:
+      outCode.add(i.name & ": " & i.ptype & ", ")
+    if cmd.arguments.len > 0:
+      outCode = outCode.substr(0, outCode.len - 3)
+    outCode.add("): " & cmd.rtn & " =\n")
+    outCode.add("  ## TODO\n") ## TODO: add actual documentation
+    outCode.add("  c" & cmd.name & "(")
+    for i in cmd.arguments:
+      outCode.add(i.name & ", ")
+    if cmd.arguments.len > 0:
+      outCode = outCode.substr(0, outCode.len - 3)
+    outCode.add(")\n")
 
   outCode.add(footer)
   writeFile("gl.nim", outCode)
